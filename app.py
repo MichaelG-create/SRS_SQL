c10d746a347a25de4911927b0a0f39e8ed086a88
micha-dv6 <michael.garcia.73@gmail.com>
2024-10-23 22:28:59 +0200

    added "duckdb query feature" to query a df

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import streamlit as st
+import pandas as pd
 import duckdb
+
+# st.write("Hello world")
+data = {"a" : [1, 2, 3], "b" : [4, 5, 6]}
+df = pd.DataFrame(data)
+
 
 tab1, tab2, tab3 = st.tabs (["cat", "dog", "fish"])
 
 with tab1:
-    st.write(input)
-    input = st.text_area(label = "type your code here")
+    sql_query = st.text_area(label = "type your code here")
+    if sql_query != "":
+        result = duckdb.sql(sql_query).df()
+    else :
+        result = pd.DataFrame({})
+    st.write(f"Vous avez entré la query suivante :{sql_query}")
 
-
-# query = (
-# """"""
-# input_user
-# """"""
-# )
-# df = duckdb.sql(query).df()
+    # 2 options pour afficher un dataframe (kifkif)
+    # st.dataframe(df)
+    # st.write(df)
+    st.dataframe(result)
