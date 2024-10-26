9591f8bfe39ac179c392b2c364e14b7e10f69106
Michael-pc <michael.garcia.73@gmail.com>
2024-10-26 09:29:43 +0200

    feature simple input answer size comparison and code refactor black pylint isort

--- app.py
+++ app.py
@@ -1,64 +1,75 @@
+# pylint: disable=missing-module-docstring
 # https://srssql.streamlit.app/
-import streamlit as st
-import pandas as pd
-import duckdb
 import io
+
+import duckdb
+import pandas as pd
+import streamlit as st
 
 # PARTIE DATA
 # ajout de vrai data pour les questions
-csv = '''
+CSV = """
 beverage,price
 orange juice,2.5
 Expresso,2
 Tea,3
-'''
-beverages  = pd.read_csv(io.StringIO(csv))
+"""
+beverages = pd.read_csv(io.StringIO(CSV))
 
-csv2 = '''
+CSV2 = """
 food_item,food_price
 cookie, 2.5
 chocolatine,2
 muffin,3
-'''
-food_items = pd.read_csv(io.StringIO(csv2))
+"""
+food_items = pd.read_csv(io.StringIO(CSV2))
 
-answer = """
+ANSWER_QUERY = """
 SELECT * FROM beverages
 CROSS JOIN food_items
 """
-solution = duckdb.sql(answer).df()
+answer_df = duckdb.sql(ANSWER_QUERY).df()
 
-#PARTIE AFFICHAGE
-#titre de la page
-st.write("""
+# PARTIE AFFICHAGE
+# titre de la page
+st.write(
+    """
 # SRS SQL 
 Spaced Repetition System SQL practice
-""")
+"""
+)
 
-#sidebar pour choisir le thème à réviser
+# sidebar pour choisir le thème à réviser
 with st.sidebar:
     option = st.selectbox(
         "What would you like to review?",
         ["Joins", "GroupBy", "Window Functions"],
         index=None,  # Default choice is None
-        placeholder="Select what you want to review"
+        placeholder="Select what you want to review",
     )
     st.write("You selected:", option)
 
 # ajout d'un header pour poser la question
 st.header("enter your code:")
-query = st.text_area(label="your SQL code here", key = "user_input")
+input_query = st.text_area(label="your SQL code here", key="user_input")
 # key = user_input sert à nommer la clé du widget (pour le retrouver après)
 
 # test si la query n'est pas vide
-if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
+if input_query:
+    input_df = duckdb.sql(input_query).df()
+    st.dataframe(input_df)
 
-# il reste à faire : tester si la réponse = la solution
+    # il reste à faire : tester si la réponse = la solution
+    # 1ere idée :
+    if len(input_df.columns) != len(answer_df.columns):
+        st.write("some columns are missing")
+
+    nb_lines_difference = answer_df.shape[0] - input_df.shape[0]
+    if nb_lines_difference != 0:
+        st.write(f"there are {nb_lines_difference} lines missing")
 
 # tabs présentant le contexte de l'exercice et la solution
-tab2, tab3 = st.tabs (["Tables", "Solution"])
+tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
     st.write("table: beverages")
@@ -66,10 +77,7 @@ with tab2:
     st.write("table: food_items")
     st.dataframe(food_items)
     st.write("expected:")
-    st.dataframe(solution)
+    st.dataframe(answer_df)
 
 with tab3:
-    st.write(answer)
-
-
-
+    st.write(ANSWER_QUERY)
