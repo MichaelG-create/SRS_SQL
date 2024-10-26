14bbf1576f4a9f8baad0233071f37fd36fb847ec
Michael-pc <michael.garcia.73@gmail.com>
2024-10-26 09:50:01 +0200

    feature full input answer comparison with errors handling

--- app.py
+++ app.py
@@ -59,14 +59,34 @@ if input_query:
     input_df = duckdb.sql(input_query).df()
     st.dataframe(input_df)
 
-    # il reste à faire : tester si la réponse = la solution
-    # 1ere idée :
-    if len(input_df.columns) != len(answer_df.columns):
-        st.write("some columns are missing")
-
     nb_lines_difference = answer_df.shape[0] - input_df.shape[0]
     if nb_lines_difference != 0:
         st.write(f"there are {nb_lines_difference} lines missing")
+
+    # il reste à faire : tester si la réponse = la solution
+    # 1ere idée : comparaison de tailles (colonnes puis lignes)
+    if len(input_df.columns) != len(answer_df.columns):
+        st.write("some columns are missing")
+
+    # on choisit de mettre les colonnes de l'answer_df dans le même ordre que celles de l'input_df
+    # pour faciliter la comparaison après
+    try:
+        input_df = input_df[answer_df.columns]
+    # bug si pas les mêmes colonnes
+    except KeyError:
+        st.write("some columns are missing")
+
+    # Après un tour dans la doc pandas
+    # nécessite que les noms de colonnes soient identiques
+    # affiche le dataframe de comparaison des dataframes input et answer
+    # probleme : si tailles des df différentes :
+    # ValueError: Can only compare identically-labeled (both index and columns) DataFrame objects
+    try:
+        st.dataframe(input_df.compare(answer_df))
+    # si ça bug sur une value error, on affiche juste son message
+    except ValueError as e:
+        st.write(f"{e}")
+
 
 # tabs présentant le contexte de l'exercice et la solution
 tab2, tab3 = st.tabs(["Tables", "Solution"])
